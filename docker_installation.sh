03c129be6bcbd09aeed74ac9b2fb1afc6dd56f2d
Kondarao1506 <Kondarao1506@gmail.com>
2026-03-16 22:46:26 +0530

    roboshop mysql changes

--- docker_installation.sh
+++ docker_installation.sh
@@ -4,15 +4,7 @@ sudo growpart /dev/xvda 4
 sudo vgs
 sudo lvextend -l +100%FREE /dev/mapper/RootVG-varVol
 sudo xfs_growfs /var
-#if [$? -ne 0]; than
-#echo "DIsk space allocation failed"
-#exit 1
-#else
-#echo "DIsk space allocation success"
-#fi
 
-sudo lsblk
-#docker installation
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
@@ -20,12 +12,7 @@ sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 
 sudo systemctl enable --now docker
 
-# if [$? -ne 0]; than
-# echo "Docker not intalled"
-# exit 1
-# else
-# echo "Docker installation  success"
-# fi
+
 
 sudo usermod -aG docker ec2-user
 
